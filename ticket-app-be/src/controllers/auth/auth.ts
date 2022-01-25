a13530d1f35d72e523aff30f81a46eba4018a645
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-01-25 16:25:30 +0800

    Login user method

--- ticket-app-be/src/controllers/auth/auth.ts
+++ ticket-app-be/src/controllers/auth/auth.ts
@@ -34,4 +34,34 @@ export class Auth {
       ctx.body = error;
     }
   }
+
+  public async login(ctx: Context): Promise<void> {
+    try {
+      const { username, password } = ctx.request.body;
+      const user = await UserModel.findOne({ username: firstLetterUppercase(username) });
+      if (!user) {
+        ctx.response.status = HTTP_STATUS.NOT_FOUND;
+        ctx.body = { message: 'User does not exist.' };
+      } else {
+        const isPasswordSame = await user.comparePassword(password);
+
+        if (!isPasswordSame) {
+          ctx.response.status = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+          ctx.body = { message: 'Password is incorrect.' };
+          return;
+        }
+
+        const userData = {
+          id: user._id,
+          username: user.username,
+        };
+
+        const token = JWT.sign({ data: userData }, 'testSecret', {});
+
+        ctx.body = { message: 'Login successfully', token };
+      }
+    } catch (error) {
+      ctx.body = error;
+    }
+  }
 }
