090f4fa951c1574b797a66b8334cf551b9522410
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-01-25 11:21:56 +0800

    Routes setup

---
+++ ticket-app-be/src/routes.ts
@@ -0,0 +1,9 @@
+import Router from 'koa-router';
+
+export function registerRoutes() {
+  const router = new Router();
+
+  //Define routers
+
+  return router;
+}
--- ticket-app-be/src/setUpServer.ts
+++ ticket-app-be/src/setUpServer.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import koaCors from 'koa2-cors';
+import { registerRoutes } from './routes';
 
 async function serverSetUp() {
   const server: Koa = new Koa();
@@ -11,6 +12,9 @@ async function serverSetUp() {
 function middleWares(server: Koa) {
   server.use(bodyParser());
   server.use(koaCors());
+
+  const routes = registerRoutes().routes();
+  server.use(routes);
 }
 
 async function startServer(server: Koa) {
