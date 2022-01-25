f6480e77bf92d7cc8414eb2bd5e38ea09b38eaf3
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-01-25 14:40:49 +0800

    Register User function

--- ticket-app-be/src/routes.ts
+++ ticket-app-be/src/routes.ts
@@ -1,9 +1,13 @@
 import Router from 'koa-router';
 
+import { Auth } from './controllers/auth/auth';
+
 export function registerRoutes() {
   const router = new Router();
 
   //Define routers
 
+  //  Auth routes
+  router.post('/register', Auth.prototype.create);
   return router;
 }
