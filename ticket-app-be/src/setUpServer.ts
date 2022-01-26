d50ce9c6f0766c9d622e830aca69d627eabaef7e
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-01-26 17:55:04 +0800

    Create socketIO connection

--- ticket-app-be/src/setUpServer.ts
+++ ticket-app-be/src/setUpServer.ts
@@ -2,6 +2,9 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import koaCors from 'koa2-cors';
 import { registerRoutes } from './routes';
+import { createServer } from 'https';
+import { Server } from 'socket.io';
+import { global } from './socket/global';
 
 async function serverSetUp() {
   const server: Koa = new Koa();
@@ -19,14 +22,14 @@ function middleWares(server: Koa) {
 
 async function startServer(server: Koa) {
   try {
+    //https://socket.io/docs/v4/server-initialization/
     const serverPort = 5000;
-
-    const serverStarted: Promise<void> = new Promise((resolve) => {
-      server.listen(serverPort, resolve);
+    const httpServer = createServer(server.callback());
+    const io = new Server(httpServer);
+    global(io);
+    httpServer.listen(serverPort, () => {
+      console.log(`Server running on port ${serverPort}`);
     });
-
-    await serverStarted;
-    console.log(`Server running on port ${serverPort}`);
   } catch (error) {
     console.log(error);
   }
