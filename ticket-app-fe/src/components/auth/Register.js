5e242ab45370d1f2dc6162ccc3a17dc26ee48d42
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-01-31 14:07:01 +0800

    onSubmit method

--- ticket-app-fe/src/components/auth/Register.js
+++ ticket-app-fe/src/components/auth/Register.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Auth.css';
 import { FormInput } from '../reusable/FormInput';
 import Button from '../reusable/Button';
 import { Link } from 'react-router-dom';
+import { RadioInput } from '../reusable/RadioInput';
 
 const Register = () => {
-  const onChange = (e) => {
-    console.log(e.target.value);
+  const [user, setUser] = useState({
+    data: { username: '', password: '', role: '' },
+  });
+  //eslint-disable-next-line
+  const [error, setError] = useState({
+    usernameError: '',
+    passwordError: '',
+    roleError: '',
+  });
+  //eslint-disable-next-line
+  const { username, password, role } = user.data;
+  //eslint-disable-next-line
+  const { usernameError, passwordError, roleError } = error;
+
+  const onSubmitRegister = (e) => {
+    e.preventDefault();
+
+    console.log(user);
   };
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    const { data } = user;
+
+    setUser({
+      data: {
+        ...data,
+        [name]: value,
+      },
+    });
+  };
+
   return (
     <div className='auth-wrapper'>
       <div className='auth-inner'>
-        <form>
+        <form onSubmit={onSubmitRegister}>
           <h3>Sign Up</h3>
           <div className='mb-3'>
             <FormInput
@@ -20,33 +50,56 @@ const Register = () => {
               label='username'
               className='form-control'
               placeholder='Enter Username'
-              value=''
+              value={username}
               error=''
               onChange={onChange}
             />
           </div>
           <div className='mb-3'>
             <FormInput
-              type='password '
+              type='password'
               name='password'
               label='password'
               className='form-control'
               placeholder='Enter password'
-              value=''
+              value={password}
               error=''
               onChange={onChange}
             />
           </div>
-          <div className='mb3'>
-            <Button
-              type='submit'
-              label='sign up'
-              className='btn btn-primary w-100'
-            />
-            <p className='forgot-password text-right'>
-              Already registered? <Link to='/sign-in'>Sign in</Link>
-            </p>
+          <div className='mb-2'>
+            <label className='form-label d-block'>Role</label>
+            <div className='form-check form-check-inline'>
+              <RadioInput
+                id='inlineRadio1'
+                name='role'
+                labelClassName='form-check-label'
+                className='form-check-input'
+                value='User'
+                error={roleError}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-check form-check-inline'>
+              <RadioInput
+                id='inlineRadio2'
+                name='role'
+                labelClassName='form-check-label'
+                className='form-check-input'
+                value='Admin'
+                error={roleError}
+                onChange={onChange}
+              />
+            </div>
           </div>
+          <Button
+            type='submit'
+            label='sign up'
+            className='btn btn-primary w-100'
+          />
+          <p className='forgot-password text-right'>
+            Already registered? <Link to='/sign-in'>Sign in</Link>
+          </p>
         </form>
       </div>
     </div>
