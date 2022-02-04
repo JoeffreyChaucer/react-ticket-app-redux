6296b3d6c87c34bb686c6befe5ceecf94ed804bf
Joeffrey Chaucer Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-02-04 18:30:45 +0800

    SetUp Redux

--- ticket-app-fe/src/App.js
+++ ticket-app-fe/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className='App container'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/sign-in' element={<Login />} />
-          <Route path='/sign-up' element={<Register />} />
-        </Routes>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='App container'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/sign-in' element={<Login />} />
+            <Route path='/sign-up' element={<Register />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
